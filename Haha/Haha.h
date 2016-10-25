bb228d5220155e30f33f6481151a9fb695498258
Peng Guo <blacksun1017@gmail.com>
2016-10-25 15:27:06 +0800

    first commit

---
+++ Haha/Haha.h
@@ -0,0 +1,19 @@
+//
+//  Haha.h
+//  Haha
+//
+//  Created by Peng Guo on 2016/10/25.
+//  Copyright © 2016年 com.iblacksun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Haha.
+FOUNDATION_EXPORT double HahaVersionNumber;
+
+//! Project version string for Haha.
+FOUNDATION_EXPORT const unsigned char HahaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Haha/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,25 @@
+source 'git@gitlab.dxy.net:ios-developer/DXYSpecs.git'
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, "9.0"
+inhibit_all_warnings!
+use_frameworks!
+
+
+def amap
+    pod 'AMap3DMap', '4.4.0'
+end
+
+
+target 'Haha' do
+    amap
+end
+
+target 'AMapDemo' do
+    amap
+end
+
+
+
+
+
